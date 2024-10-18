d460ebccc45639313e67ac3e05188af3c1bd9e09
VladimirKomov <v.e.komov@gmail.com>
2024-10-18 19:22:54 +0300

    Initialize Django project with JWT auth

---
+++ backend/config/app/views/user_views.py
@@ -0,0 +1,28 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from ..serializers.user_serializers import UserSerializer
+
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        # user into serializer
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # if OK
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # if errors
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLogoutView(APIView):
+    def post(self, request):
+        try:
+            request_token = request.data['refresh_token']
+            token = RefreshToken(request_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
