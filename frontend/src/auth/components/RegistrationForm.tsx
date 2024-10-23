015cb2605b26638b22ab40026bebe1a977ca5454
VladimirKomov <v.e.komov@gmail.com>
2024-10-23 09:39:45 +0300

    Implement success message post-registration

--- frontend/src/auth/components/RegistrationForm.tsx
+++ frontend/src/auth/components/RegistrationForm.tsx
@@ -3,6 +3,7 @@ import styles from "./RegistrationForm.module.css";
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch, RootState} from "../../store.ts";
 import {register} from "../redux/authSlice.ts";
+import {useNavigate} from 'react-router-dom';
 
 const RegistrationForm: React.FC = () => {
     const [username, setUsername] = useState('');
@@ -12,9 +13,11 @@ const RegistrationForm: React.FC = () => {
     const [password, setPassword] = useState('');
     const [password2, setPassword2] = useState('');
     const [passwordMismatch, setPasswordMismatch] = useState(false);
-    const [passworError, setPasswordError] = useState('');
-    const {loading, error} = useSelector((state: RootState) => state.auth);
+    const [errorForm, setErrorForm] = useState('');
+    const {loading, error: errorApi} = useSelector((state: RootState) => state.auth);
+    const [isRegistered, setIsRegistered] = useState(false);
     const dispatch = useDispatch<AppDispatch>();
+    const navigate = useNavigate();
 
     //checking the password match
     const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,91 +34,107 @@ const RegistrationForm: React.FC = () => {
         e.preventDefault();
         //check password before the sending
         if (passwordMismatch) {
-            setPasswordError('Passwords do not match');
+            setErrorForm('Passwords do not match');
             return;
         }
-        dispatch(register({ username, email, firstName, lastName, password, password2 }));
-        setPasswordError('');
+        dispatch(register({username, email, firstName, lastName, password, password2}))
+            .unwrap()
+            .then(() => {
+                setErrorForm('');
+                setIsRegistered(true); // Устанавливаем, что регистрация успешна
+            })
     }
 
 
     return (
-        <form className={styles.form} onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Name:</label>
-                <input
-                    className={styles.input}
-                    type="text"
-                    id="name"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="email">Email:</label>
-                <input
-                    className={styles.input}
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="firstName">First name:</label>
-                <input
-                    className={styles.input}
-                    type="text"
-                    id="firstName"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="lastName">Last name:</label>
-                <input
-                    className={styles.input}
-                    type="text"
-                    id="lastName"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input
-                    className={styles.input}
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="password2">Confirm Password:</label>
-                <input
-                    className={styles.input}
-                    type="password"
-                    id="password2"
-                    value={password2}
-                    onChange={handlePassword2Change}
-                    required
-                />
-            </div>
-            {/* Password mismatch message */}
-            {passwordMismatch && <p className={styles.error}>Passwords do not match</p>}
-            {/* Error message */}
-            {error && <p className={styles.error}>{passworError}</p>}
-            {error && <p className={styles.error}>{error.message}</p>}
-            <button className={styles.button} type="submit" disabled={loading}>
-                {loading ? 'Loading...' : 'Register'}
-            </button>
-        </form>
+        <div>
+            {!isRegistered ? (
+                <form className={styles.form} onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="name">Name:</label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            id="name"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            className={styles.input}
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="firstName">First name:</label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            id="firstName"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="lastName">Last name:</label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            id="lastName"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            className={styles.input}
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password2">Confirm Password:</label>
+                        <input
+                            className={styles.input}
+                            type="password"
+                            id="password2"
+                            value={password2}
+                            onChange={handlePassword2Change}
+                            required
+                        />
+                    </div>
+                    {/* Password mismatch message */}
+                    {passwordMismatch && <p className={styles.error}>Passwords do not match</p>}
+                    {/* Error message */}
+                    {errorApi && <p className={styles.error}>{errorForm}</p>}
+                    {errorApi && <p className={styles.error}>{errorApi.message}</p>}
+                    <button className={styles.button} type="submit" disabled={loading}>
+                        {loading ? 'Loading...' : 'Register'}
+                    </button>
+                </form>
+            ) : (
+                <div className={styles.successContainer}>
+                    <h2 className={styles.successMessage}>Регистрация успешна!</h2>
+                    <button className={styles.successButton}
+                            onClick={() => navigate('/')}>
+                        Вернуться на главную
+                    </button>
+                </div>
+            )}
+        </div>
     );
 }
 
