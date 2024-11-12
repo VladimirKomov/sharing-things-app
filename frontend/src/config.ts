405d69927751ebf208e0dc786d5a6009d3df6375
VladimirKomov <v.e.komov@gmail.com>
2024-11-12 07:47:54 +0200

    Disable CORS for all origins and remove CSRF middleware
    Removed CORS_ALLOW_ALL_ORIGINS to restrict cross-origin requests to specified domains and eliminated CsrfViewMiddleware from the middleware stack. This change impacts the backend's security settings and cross-origin communication policies.

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'https://backend-axmi.onrender.com/api/';
+// export const API_BASE_URL = 'https://backend-axmi.onrender.com/api/';
+export const API_BASE_URL = 'http://localhost:8000/api/';
 
 //for log
 import log from 'loglevel';
